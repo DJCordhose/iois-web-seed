aded8ae5ae752445614c98c6e55907571eaf8595
Oliver Zeigermann <oliver.zeigermann@gmail.com>
2015-06-05 14:43:10 +0200

    added a bit more jquery to update send button state

--- local-mongo-url.js
+++ local-mongo-url.js
@@ -1,1 +1,5 @@
+// requires to start local mongodb
+// on Mac this might look like this
+// mongod --syslog --fork --dbpath /usr/local/share/mongodb/data
+
 module.exports = 'mongodb://localhost/sandbox';
--- public/main.js
+++ public/main.js
@@ -1,10 +1,18 @@
 $(function () {
+    function updateSendButtonState() {
+        var value = inputField.val();
+        $('.send').prop("disabled", value.length === 0);
+    }
+
     var inputField = $('.greeting-form input[name="greeting"]');
 
     inputField.focus();
 
     inputField.on('keyup', function (e) {
+        updateSendButtonState();
         $('.greeting-preview').text(inputField.val());
     });
+
+    updateSendButtonState();
 });
 
